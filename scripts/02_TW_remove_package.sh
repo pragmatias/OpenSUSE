9f05e2a7cb2012bd4b1efc326ca9538c28943f12
gmatias <gregory.matias@mousquetaires.com>
2019-03-27 15:30:40 +0100

    maj script

--- scripts/02_TW_remove_package.sh
+++ scripts/02_TW_remove_package.sh
@@ -12,7 +12,7 @@ pkg_list_remove=$(egrep -v '(^\#)|(^\s+$)' $CWD/../config/pkglists/package_remov
 for pkg_remove in ${pkg_list_remove}; do
   if rpm -q ${pkg_remove} 2>&1 > /dev/null ; then
 	exec_cmd="zypper --non-interactive remove --clean-deps ${pkg_remove}"
-	msg_log="deleting the package $PAQUET"
+	msg_log="deleting the package ${pkg_remove}"
 	logMessage "-1" "${msg_log}... [${exec_cmd}]"
 	${exec_cmd} >> $LOG 2>&1
 	logMessage "${?}" "${msg_log}"
