7c259b8db265b691bd483c4ed4befd607d5ba9ec
gmatias <gregory.matias@mousquetaires.com>
2019-05-14 15:09:34 +0200

    test install on vm

---
+++ scripts/06_Leap_config_system.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Sytem config
+
+. 00_functions_install.sh
+
+# Force the OnlyRequires mode for Zypper
+sed -i 's@solver.OnlyRequires = false@solver.OnlyRequires = true@' /etc/zypp/zypp.conf
+
+# Modify the display manager to LightDM
+sed -i 's@DISPLAYMANAGER=".*"@DISPLAYMANAGER="lightdm"@' /etc/sysconfig/displaymanager
+
+# Modify the graphical.targer
+systemctl set-default graphical.target
