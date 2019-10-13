d3fd14f1c1cb060ca4f2bdcb3162c6a0c44b56c9
pragmatias <gregory.matias@pragmatias.fr>
2019-10-13 13:09:04 +0200

    test rofi conf

--- config/polybar/menu_full.sh
+++ config/polybar/menu_full.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -modi run,drun,window,ssh -show run -fullscreen True -lines 20 -line-margin 6 -padding 200 -bw 0 -sidebar-mode -font "Ubuntu Nerd Font 10
+rofi -modi run,drun,window,ssh -show run -fullscreen True -lines 20 -line-margin 6 -padding 200 -bw 0 -sidebar-mode -font "Ubuntu Nerd Font 10"
