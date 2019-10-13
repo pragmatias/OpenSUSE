6510ae822c04063d8e57f6d65412b49855d76f60
pragmatias <gregory.matias@pragmatias.fr>
2019-10-13 13:06:32 +0200

    test rofi conf

--- config/polybar/power_menu.sh
+++ config/polybar/power_menu.sh
@@ -2,7 +2,7 @@
 
 ## Created By Aditya Shakya
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'System' -location 3 -yoffset 25  -width 10 -hide-scrollbar -line-padding 4 -padding 10 -lines 4 -font "Ubuntu Nerd Font 9" <<< "⏼ Lock| Logout| Reboot|⏻ Shutdown")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'System' -location 3 -yoffset 25  -width 10 -hide-scrollbar -line-padding 4 -padding 10 -lines 4 -font "SF Compact Display 12" <<< "⏼ Lock| Logout| Reboot|⏻ Shutdown")"
             case "$MENU" in
                 *Lock) cd ~/.config/polybar && ./i3lock_fancy.sh ;;
                 *Logout) i3-msg exit ;;
