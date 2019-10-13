dbb2ea2e31ee319ef9ebd61f0ad4ba9f8c98a5ef
pragmatias <gregory.matias@pragmatias.fr>
2019-10-13 13:16:15 +0200

    test rofi conf

--- config/polybar/menu_table.sh
+++ config/polybar/menu_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -modi run,drun,window -theme ~/.config/rofi/gruvbox-dark.rasi -lines 10 -padding 20 -width 50  -location 1 -yoffset 25 -xoffset 0 -show drun -sidebar-mode -columns 3 -font "SF Compact Display, Ubuntu Nerd Font 9"
+rofi -modi drun,run,window -theme ~/.config/rofi/gruvbox-dark.rasi -lines 10 -padding 20 -width 50  -location 1 -yoffset 25 -xoffset 0 -show drun -sidebar-mode -columns 3 -font "SF Compact Display, Ubuntu Nerd Font 9"
