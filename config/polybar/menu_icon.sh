3c718e720963325ec69066248d48fa04e5cf225a
pragmatias <gregory.matias@pragmatias.fr>
2019-10-13 13:18:27 +0200

    test rofi conf

--- config/polybar/menu_icon.sh
+++ config/polybar/menu_icon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -modi drun,run -show drun -location 1 -yoffset 25 -xoffset 0 -line-padding 4 -columns 1 -width 25 -lines 10 -padding 25 -show-icons -sidebar-mode -drun-icon-theme "Papirus" -font "SF Compact Display, Ubuntu Nerd Font 9"
+rofi -modi drun,run -show drun -theme ~/.config/rofi/gruvbox-dark.rasi -location 1 -yoffset 25 -xoffset 0 -line-padding 4 -columns 1 -width 25 -lines 10 -padding 25 -show-icons -sidebar-mode -drun-icon-theme "Papirus" -font "SF Compact Display, Ubuntu Nerd Font 9"
